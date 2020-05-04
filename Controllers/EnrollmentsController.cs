9131f04df51dd1c941b1d0c07eacd69c75d373ce
johan mauricio chavez montoya <johanmauricio10@gmail.com>
2020-05-04 11:47:16 -0500

    fin clase 3o Abril

--- Controllers/EnrollmentsController.cs
+++ Controllers/EnrollmentsController.cs
@@ -10,10 +10,16 @@ namespace ContosoUniversity.Controllers
     public class EnrollmentsController : Controller
     {
         private IEnrollmentService enrollmentService;
+        private IStudentService studentService;
+        private ICourseService courseService;
 
-        public EnrollmentsController(IEnrollmentService enrollmentService)
+        public EnrollmentsController(IEnrollmentService enrollmentService,
+                                     IStudentService studentService,
+                                     ICourseService courseService)
         {
             this.enrollmentService = enrollmentService;
+            this.studentService = studentService;
+            this.courseService = courseService;
         }
 
         public async Task<IActionResult> Index()
@@ -22,5 +28,20 @@ namespace ContosoUniversity.Controllers
 
             return View(listEnrollments);
         }
+
+        public async Task<ActionResult> Create() {
+
+            ViewBag.Courses = await courseService.GetAll();
+            ViewBag.Students = await studentService.GetAll();
+
+            List<string> ListGreades = new List<string> { "A", "B", "C", "D", "E", "F" };
+
+
+            ViewBag.Grades = ListGreades;
+
+            return View();
+        
+        }
+
     }
 }
